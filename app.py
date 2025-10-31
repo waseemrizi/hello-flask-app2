855f3f97fc4a49bb2f66008a53373f764fda21e9
waseemrizi <waseemrizi@outlook.com>
2025-11-01 01:39:01 +0500

    Added current server time display to Flask app

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from K3s via Jenkins (New App)!"
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"<h2>Hello from Flask App 2!</h2><p>🕒 Current server time: {current_time}</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
